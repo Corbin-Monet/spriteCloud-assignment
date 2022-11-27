118236fa03f25ac9c948a0e3b13dcc92742a6ca9
Corbz <corbz@DESKTOP-ONETNKG.localdomain>
2022-11-27 21:55:57 +0000

    Fixing some errors in code

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -2,16 +2,6 @@
 /**
  * @type {Cypress.PluginConfig}
  */
-import "./commands";
-import addContext from "mochawesome/addContext";
-
-Cypress.on("test:after:run", (test, runnable) => {
-  let videoName = Cypress.spec.name;
-  videoName = videoName.replace("/.js.*", ".js");
-  const videoUrl = "videos/" + videoName + ".mp4";
-
-  addContext({ test }, videoUrl);
-});
 
 let loginName;
 
